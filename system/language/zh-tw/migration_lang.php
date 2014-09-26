11d85983569a06f7a6843c082a97c7e8b26f6a5a
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-09-26 20:39:22 +0800

    add chinese language
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ system/language/zh-tw/migration_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['migration_none_found'] = "沒有發現任何 Migrations";
+$lang['migration_not_found'] = "此 Migration 不存在";
+$lang['migration_multiple_version'] = "多重 migrations 擁有相同的版本號碼: %d";
+$lang['migration_class_doesnt_exist'] = "Migration 類別 \"%s\" 不存在";
+$lang['migration_missing_up_method'] = "Migration 類別 \"%s\" i缺少 'up' 方法";
+$lang['migration_missing_down_method'] = "Migration 類別 \"%s\" 缺少 'down' 方法";
+$lang['migration_invalid_filename'] = "Migration \"%s\" 包含無效的欄位名稱";
+
+
+/* End of file migration_lang.php */
+/* Location: ./system/language/zh-TW/migration_lang.php */
---
+++ system/language/zh-tw/profiler_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['profiler_database'] = '資料庫';
+$lang['profiler_controller_info'] = '類別/方法';
+$lang['profiler_benchmarks'] = '評測';
+$lang['profiler_queries'] = '指令';
+$lang['profiler_get_data'] = 'GET 資料';
+$lang['profiler_post_data'] = 'POST 資料';
+$lang['profiler_uri_string'] = 'URI 字串';
+$lang['profiler_memory_usage'] = '記憶體使用';
+$lang['profiler_config'] = '設定變數';
+$lang['profiler_session_data']	= 'SESSION 資料';
+$lang['profiler_headers'] = 'HTTP標頭';
+$lang['profiler_no_db'] = '目前無法載入資料庫驅動';
+$lang['profiler_no_queries'] = '沒有可執行指令';
+$lang['profiler_no_post'] = '沒有 POST 資料存在';
+$lang['profiler_no_get'] = '沒有 GET 資料存在';
+$lang['profiler_no_uri'] = '沒有 URI 字串存在';
+$lang['profiler_no_memory'] = '無法取得記憶體使用';
+$lang['profiler_no_profiles'] = '無性能分析資料 - 所有性能分析工具區塊已經被關閉.';
+$lang['profiler_section_hide']	= '隱藏';
+$lang['profiler_section_show']	= '顯示';
+
+/* End of file profiler_lang.php */
+/* Location: ./system/language/zh-TW/profiler_lang.php */
